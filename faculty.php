100510a6d036f01a7096a55a29edb6bc07332db5
sai sampath k <sskolla0797@gmail.com>
2018-11-11 13:08:37 +0530

    Update faculty.php

--- faculty.php
+++ faculty.php
@@ -89,28 +89,27 @@ while ($row = mysqli_fetch_array($result)) { ?>
 	<thead>
 		<tr>
 <th width="10%"><b><input type="submit" name="accept" value="acc"><b></th>
-	<th>Room Number</th>
-	<th>date</th>
-	<th>Begining</th>
-	<th>ending</th>
+   <th>date for req</th>
+	<th>Room Number for req</th>
+	<th>Begining for req</th>
+	<th>date tobe swap</th>
+   <th>Room Number tobe swap</th>
+	<th>Begining tobe swap</th>
 </tr>
 </thead>
 <?php 
 $d=$_SESSION['srcfno'];
-$w="select srcfno,srcsno,dessno,desfno from swap where app=2 and desfno='$d'";//2 means still in waiting
+$w="select srcdate as a,srcbegining as b,srcroomno as c,srcsno as d,desdate as e,desbegining as f,desroomno as g from swap where app=2 and desfno='$d'";//2 means still in waiting
  $resultz = mysqli_query($db, $w);
 while ($row1 = mysqli_fetch_array($resultz)) { ?>
-    <?php
-	$h="SELECT session.roomno as a,session.date as i,session.begining as b,session.ending as c,session.sno as s,teacher.fno as f from session INNER JOIN teacher where  teacher.fno=session.invig_duty and teacher.fno='".$row1['srcfno']."' and session.sno='".$row1['srcsno']."'";
-	 $result1 = mysqli_query($db, $h);?>
-	 <h1><?php echo "Requests  Swapping duty"; ?></h1>
-	 <?php while ($row2 = mysqli_fetch_array($result1)) { ?>
 			<tr>
-			<td><input type="checkbox" name="chk[]"  class="checkbox" value='<?php echo $row1['srcsno']; ?>'/></td>
-		<td><?php echo $row2['a']; ?></td>
-		<td><?php echo $row2['i']; ?></td>
-		<td><?php echo $row2['b']; ?></td>
-		<td><?php echo $row2['c']; ?></td>
+			<td><input type="checkbox" name="chk[]"  class="checkbox" value='<?php echo $row1['d']; ?>'/></td>
+		<td><?php echo $row1['a']; ?></td>
+		<td><?php echo $row1['c']; ?></td>
+		<td><?php echo $row1['b']; ?></td>
+		<td><?php echo $row1['e']; ?></td>
+		<td><?php echo $row1['g']; ?></td>
+		<td><?php echo $row1['f']; ?></td>
 		</tr>
 <?php }} ?>
 </body>
