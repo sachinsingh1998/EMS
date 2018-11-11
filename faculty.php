0d9c9c7f9860b75c63ecee4bcd069012551b063a
sai sampath k <sskolla0797@gmail.com>
2018-11-11 16:26:35 +0530

    Update faculty.php

--- faculty.php
+++ faculty.php
@@ -23,17 +23,52 @@ td{
     padding-bottom: 90px;
 }
 tr:nth-child(even) {background-color: #f2f2f2;}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    background-color: #111;
+}
+
+.active {
+    background-color: #4CAF50;
+}
 </style>
 </head>
-<body>
+<body background="bg.jpg">
+<ul>
+  <li style="float:right"><a class="active" href="facultypage.php">Logout</a></li>
+</ul>
 <div class="container">
 <h2><center><u>Duty Allocated</u></center></h2>
 <?php
 
  session_start();
  $name=$password="";
-$name=$_POST["name"];
-$password=$_POST["pass"];
+if(isset($_POST["name"])){
+ $_SESSION['name']=$_POST["name"];
+$_SESSION['pass']=$_POST["pass"];
+ }
+$name= $_SESSION['name'];
+$password=$_SESSION['pass'];
+
 $ivemail="";
 $invpas="";
 
@@ -55,6 +90,7 @@ echo	"Invalid name or password";
 	$row="SELECT session.roomno as a,session.date as i,session.begining as b,session.ending as c,session.sno as s,teacher.fno as f from session INNER JOIN teacher where teacher.fno=session.invig_duty and teacher.fname='$name'";
  $result = mysqli_query($db, $row);
  ?>
+
      <form method="POST" action="faculty1.php">
 <table>
 	<thead>
@@ -84,7 +120,19 @@ while ($row = mysqli_fetch_array($result)) { ?>
 <?php 
 
  ?>
+  <h2><center><u>Swapping Request</u></center></h2>
       <form method="POST" action="faculty2.php">
+
+<?php 
+$d=$_SESSION['srcfno'];
+$w="select srcdate as a,srcbegining as b,srcroomno as c,srcsno as d,desdate as e,desbegining as f,desroomno as g from swap where app=2 and desfno='$d'";//2 means still in waiting
+ $resultz = mysqli_query($db, $w);
+   $count = mysqli_num_rows($resultz);
+   if($count==0)
+   {
+	   echo "<h2><center>Swap request empty</center> </h2>";
+   }
+   else{?>
 <table>
 	<thead>
 		<tr>
@@ -92,16 +140,12 @@ while ($row = mysqli_fetch_array($result)) { ?>
    <th>date for req</th>
 	<th>Room Number for req</th>
 	<th>Begining for req</th>
-	<th>date tobe swap</th>
-   <th>Room Number tobe swap</th>
+	<th>date to be swap</th>
+   <th>Room Number to be swap</th>
 	<th>Begining tobe swap</th>
 </tr>
 </thead>
-<?php 
-$d=$_SESSION['srcfno'];
-$w="select srcdate as a,srcbegining as b,srcroomno as c,srcsno as d,desdate as e,desbegining as f,desroomno as g from swap where app=2 and desfno='$d'";//2 means still in waiting
- $resultz = mysqli_query($db, $w);
-while ($row1 = mysqli_fetch_array($resultz)) { ?>
+<?php while ($row1 = mysqli_fetch_array($resultz)) { ?>
 			<tr>
 			<td><input type="checkbox" name="chk[]"  class="checkbox" value='<?php echo $row1['d']; ?>'/></td>
 		<td><?php echo $row1['a']; ?></td>
@@ -111,6 +155,6 @@ while ($row1 = mysqli_fetch_array($resultz)) { ?>
 		<td><?php echo $row1['g']; ?></td>
 		<td><?php echo $row1['f']; ?></td>
 		</tr>
-<?php }} ?>
+   <?php } }?>
 </body>
 </html>
